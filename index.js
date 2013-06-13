986f8c733d3ec78b157bf3e74497be372b6d5e62
Mariusz Nowak <mariusz@medikoo.com>
2013-06-13 18:09:03 +0200

    Add default value getter/setter that emits change

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 'use strict';
 
-var ee = require('event-emitter/lib/core')
+var d = require('es5-ext/lib/Object/descriptor')
+  , ee = require('event-emitter/lib/core')
 
   , defineProperty = Object.defineProperty
-  , dscr = { value: true, enumerable: false, configurable: true,
-		writable: true };
+  , idDscr = d(true)
+  , _valueDscr = d()
+
+  , valueDscr, Mutable;
+
+valueDscr = d.gs('ec', function () { return this._value; }, function (nu) {
+	var old = this._value;
+	if (!this.hasOwnProperty('_value')) {
+		_valueDscr.value = nu;
+		defineProperty(this, '_value', _valueDscr);
+	} else {
+		this._value = nu;
+	}
+	if (nu !== old) this.emit('change', nu);
+});
+
+Mutable = function () {};
+Object.defineProperties(ee(Mutable.prototype), {
+	_isMutableEmitterValue_: idDscr,
+	value: valueDscr,
+	toString: d(function () { return String(this._value); })
+});
 
 module.exports = function (value) {
-	if (value == null) value = {};
+	if (value == null) return new Mutable();
 	if (!value.on || !value.off) ee(value);
-	return defineProperty(value, '_isMutableEmitterValue_', dscr);
+	if (!('value' in value)) defineProperty(value, 'value', valueDscr);
+	return defineProperty(value, '_isMutableEmitterValue_', idDscr);
 };
--- test/index.js
+++ test/index.js
@@ -5,17 +5,17 @@ var is = require('../is');
 module.exports = function (t, a) {
 	var value = t(), invoked = false, x = {};
 	a(is(value), true, "Create");
-	value.on('change', function () { invoked = true; });
+	value.on('change', function (val) { invoked = val; });
 	a(invoked, false, "Pre emit");
-	value.emit('change');
-	a(invoked, true, "After emit");
+	value.value = 'raz';
+	a(invoked, 'raz', "After emit");
 
 	invoked = false;
 	value = t(x);
 	a(x, value, "Extend");
 	a(is(value), true, "No args");
-	value.on('change', function () { invoked = true; });
+	value.on('change', function (val) { invoked = val; });
 	a(invoked, false, "Pre emit");
-	value.emit('change');
-	a(invoked, true, "After emit");
+	value.value = 'dwa';
+	a(invoked, 'dwa', "After emit");
 };
