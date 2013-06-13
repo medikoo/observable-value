1ba4bdbe77746b8dae6db82441f02a16f1987398
Mariusz Nowak <mariusz@medikoo.com>
2013-06-13 18:19:45 +0200

    Eq utility

---
+++ eq.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var isEqual    = require('es5-ext/lib/Object/is')
+  , mutable    = require('./')
+  , isMutable  = require('./is');
+
+module.exports = function (left, right) {
+	var isLMutable, isRMutable, value;
+	isLMutable = isMutable(left);
+	isRMutable = isMutable(right);
+	if (!isLMutable && !isRMutable) return isEqual(left, right);
+	if (isLMutable) {
+		left.on('change', function (l) {
+			value.value = isEqual(l, isRMutable ? right.value : right);
+		});
+	}
+	if (isRMutable) {
+		right.on('change', function (r) {
+			value.value = isEqual(isLMutable ? left.value : left, r);
+		});
+	}
+	value = mutable();
+	value.value = isEqual(isLMutable ? left.value : left,
+		isRMutable ? right.value : right);
+	return value;
+};
---
+++ test/eq.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var isMutable = require('../is')
+  , mutable   = require('../');
+
+module.exports = function (t, a) {
+	var x = mutable(), y = mutable(), r, ev;
+	a(t('raz', 'raz'), true, "Immutable");
+	a(t(), true, "Undefined");
+	a(t('raz'), false, "Immutable: not equal");
+
+	a(isMutable(r = t(x, 'raz')), true, "Left");
+	a(r.value, false, "Left: value");
+	r.on('change', function (val) { ev = val; });
+	x.value = 'raz';
+	a(ev, true, "Left: Equalized");
+	a(r.value, true, "Left: Equalized: value");
+	x.value = 'dwa';
+	a(ev, false, "Left: Disequalized");
+	a(r.value, false, "Left: Disequalized: value");
+
+	ev = null;
+	a(isMutable(r = t('dwa', x)), true, "Right");
+	a(r.value, true, "Right: value");
+	r.on('change', function (val) { ev = val; });
+	x.value = 'raz';
+	a(ev, false, "Right: Disqualized");
+	a(r.value, false, "Right: Disqualized: value");
+	x.value = 'dwa';
+	a(ev, true, "Right: Equalized");
+	a(r.value, true, "Right: Equalized: value");
+
+	a(isMutable(r = t(x, y)), true, "Both");
+	a(r.value, false, "Both: value");
+	r.on('change', function (val) { ev = val; });
+	y.value = 'dwa';
+	a(ev, true, "Both: Equalized");
+	a(r.value, true, "Both: Equalized: value");
+	x.value = 'raz';
+	a(ev, false, "Both: Disequalized");
+	a(r.value, false, "Both: Disequalized: value");
+};
