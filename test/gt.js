5c90dbcbb69cd34bcca52c089368d7e809057bff
Mariusz Nowak <mariusz@medikoo.com>
2013-09-06 16:34:57 +0200

    `gt` greater than, mutable

---
+++ test/gt.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var isMutable = require('../is')
+  , Mutable   = require('../');
+
+module.exports = function (t, a) {
+	var x = new Mutable(), y = new Mutable(), r, ev;
+	a(t(23, 10), true, "Immutable");
+	a(t(), false, "Undefined");
+	a(t('raz'), false, "Immutable: one argument");
+	a(t(20, 34), false, "Immutable: less than");
+
+	a(isMutable(r = t(x, 15)), true, "Left");
+	a(r.value, false, "Left: value");
+	r.on('change', function (val) { ev = val; });
+	x.value = 20;
+	a(ev, true, "Left: Equalized");
+	a(r.value, true, "Left: Equalized: value");
+	x.value = 15;
+	a(ev, false, "Left: Disequalized");
+	a(r.value, false, "Left: Disequalized: value");
+
+	ev = null;
+	a(isMutable(r = t(20, x)), true, "Right");
+	a(r.value, true, "Right: value");
+	r.on('change', function (val) { ev = val; });
+	x.value = 30;
+	a(ev, false, "Right: Disqualized");
+	a(r.value, false, "Right: Disqualized: value");
+	x.value = 19;
+	a(ev, true, "Right: Equalized");
+	a(r.value, true, "Right: Equalized: value");
+
+	a(isMutable(r = t(x, y)), true, "Both");
+	a(r.value, false, "Both: value");
+	r.on('change', function (val) { ev = val; });
+	y.value = 17;
+	a(ev, true, "Both: Equalized");
+	a(r.value, true, "Both: Equalized: value");
+	x.value = 16;
+	a(ev, false, "Both: Disequalized");
+	a(r.value, false, "Both: Disequalized: value");
+};
