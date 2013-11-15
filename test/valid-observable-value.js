a30cfde37fa3184ca142eadd97299a43cb221bc7
Mariusz Nowak <mariusz@medikoo.com>
2013-11-15 14:59:05 +0100

    validObservableValue

---
+++ test/valid-observable-value.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var ObservableValue = require('../value');
+
+module.exports = function (t, a) {
+	var observable;
+	a.throws(function () { t(undefined); }, TypeError, "Undefined");
+	a.throws(function () { t(null); }, TypeError, "Null");
+	a.throws(function () { t(true); }, TypeError, "Primitive");
+	a.throws(function () { t('raz'); }, TypeError, "String");
+	a.throws(function () { t({}); }, TypeError, "Object");
+	a.throws(function () { t([]); }, TypeError, "Array");
+	a.throws(function () { t(function () {}); }, TypeError, "Function");
+	observable = new ObservableValue();
+	a(t(observable), observable, "Observable");
+};
