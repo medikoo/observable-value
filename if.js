fa4d6c240369cac84a4d9c7c96dd113ad3d91ab3
Mariusz Nowak <medyk@medikoo.com>
2015-03-19 12:42:58 +0100

    Support function resolvers in if

--- if.js
+++ if.js
@@ -1,32 +1,33 @@
 'use strict';
 
 var Observable   = require('./')
-  , isObservable = require('./is-observable-value');
+  , isObservable = require('./is-observable-value')
+
+  , resolveArgument = function (arg) { return (typeof arg === 'function') ? arg() : arg; };
 
 module.exports = function (cond, onTrue, onFalse) {
-	var isTObservable, isFObservable, value;
+	var observable, isTrueResolved, isFalseResolved, update;
 	if (!isObservable(cond)) return cond ? onTrue : onFalse;
-
-	isTObservable = isObservable(onTrue);
-	isFObservable = isObservable(onFalse);
-	if (isTObservable) {
-		onTrue.on('change', function (event) {
-			if (!cond.value) return;
-			value.value = event.newValue;
-		});
-	}
-	if (isFObservable) {
-		onFalse.on('change', function (event) {
-			if (cond.value) return;
-			value.value = event.newValue;
-		});
-	}
-	cond.on('change', function (event) {
-		if (event.newValue) value.value = isTObservable ? onTrue.value : onTrue;
-		else value.value = isFObservable ? onFalse.value : onFalse;
-	});
-	if (cond.value) value = isTObservable ? onTrue.value : onTrue;
-	else value = isFObservable ? onFalse.value : onFalse;
-	value = new Observable(value);
-	return value;
+	observable = new Observable();
+	update = function (value) {
+		if (value) {
+			if (isTrueResolved) {
+				value = onTrue;
+			} else {
+				value = onTrue = resolveArgument(onTrue);
+				isTrueResolved = true;
+			}
+		} else {
+			if (isFalseResolved) {
+				value = onFalse;
+			} else {
+				value = onFalse = resolveArgument(onFalse);
+				isFalseResolved = true;
+			}
+		}
+		observable.value = value;
+	};
+	update(cond.value);
+	cond.on('change', function (event) { update(event.newValue); });
+	return observable;
 };
